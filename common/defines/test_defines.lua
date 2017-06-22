0adc57c00e110460bcd8fff6bf57b71695105dbe
Richard Bong <trmilescontact@gmail.com>
2017-06-22 10:59:51 -0500

    Uploading Version 1.2.2

---
+++ common/defines/test_defines.lua
@@ -0,0 +1,3 @@
+--Hello, if you're looking to put this in a modpack, ask and link, but jokes on you, it's compatible in theory with literally every mod so you shouldn't have to put it in a modpack!
+--this is a test
+NDefines.NAI.ESTIMATED_CONVOYS_PER_DIVISION = 6
